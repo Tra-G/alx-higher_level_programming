8b1f383b4890d2d603e535fd6c438c337c729e54
Tracy <okorietracy8@gmail.com>
2022-09-19 20:53:59 -0700

    Hello world code using pyscript

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys
+omotayo = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write(omotayo)
+sys.exit(1)
---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+Directory for the first Project on Python
